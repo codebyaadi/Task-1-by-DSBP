ab4ac7b87b9934ff8f3dbb6f831076f5d287c3cf
Aditya Rajbhar <100792725+codebyaadi@users.noreply.github.com>
2024-01-04 18:50:50 +0530

    Update toggle-button.jsx 2

--- src/components/navbar/sidebar/toggle-button/toggle-button.jsx
+++ src/components/navbar/sidebar/toggle-button/toggle-button.jsx
@@ -8,6 +8,7 @@ const ToggleButton = ({ setOpen }) => {
           strokeWidth="3"
           stroke="black"
           strokeLinecap="round"
+          d="M 2 9.423 L 20 9.423"
           variants={{
             closed: { d: "M 2 2.5 L 20 2.5" },
             open: { d: "M 3 16.5 L 17 2.5" },
@@ -27,6 +28,7 @@ const ToggleButton = ({ setOpen }) => {
           strokeWidth="3"
           stroke="black"
           strokeLinecap="round"
+          d="M 2 9.423 L 20 9.423"
           variants={{
             closed: { d: "M 2 16.346 L 20 16.346" },
             open: { d: "M 3 2.5 L 17 16.346" },
